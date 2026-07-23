fceeb439b2c1a2ea4398b5f0ba5093c5906d9f8a
alex-mountain <alexandermountain@gmail.com>
2026-07-23 13:43:15 -0700

    chore(tracker): remove comment

--- app/challenge/[id].tsx
+++ app/challenge/[id].tsx
@@ -46,7 +46,6 @@ export default function ChallengeScreen() {
         </Pressable>
       </View>
 
-      {/* Content */}
       <View style={styles.content}>
         {activeTab === "first" ? (
           <ChallengeCategoryList challengeId={challengeId} />
