5350e422d668896dd3dfe7a97a219031eee5c645
alex-mountain <alexandermountain@gmail.com>
2026-07-27 09:16:34 -0700

    hotfix(theme): light mode theme clashes with hard-coded styles
    - consolidation of themes is a TODO for later. For now just force dark theme so everything is visible.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,18 @@
 import { migrateDb } from "@/db/schema";
-import { useColorScheme } from "@/hooks/use-color-scheme";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
+import { DarkTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
 import { SQLiteProvider } from "expo-sqlite";
 import "react-native-reanimated";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  // const colorScheme = useColorScheme();
 
   return (
     <SQLiteProvider
       databaseName="reading-challenge-tracker.db"
       onInit={migrateDb}
     >
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <ThemeProvider value={DarkTheme}>
         <Stack>
           <Stack.Screen name="index" options={{ title: "Challenges" }} />
         </Stack>
