d84b5f263552a8ec5aec0acc95d5bf0f2c1b18ed
alex-mountain <alexandermountain@gmail.com>
2026-08-13 14:13:28 -0700

    feat(theme): force dark theme with new themeprovider
    - light styling isn't ready yet

--- constants/theme/ThemeProvider.tsx
+++ constants/theme/ThemeProvider.tsx
@@ -1,21 +1,19 @@
 import { Themes, type Theme } from "@/constants/theme/theme";
 import { createContext, useContext, useMemo, type ReactNode } from "react";
-import { useColorScheme } from "react-native";
 
 const ThemeContext = createContext<Theme | null>(null);
 
 type ThemeProviderProps = {
   children: ReactNode;
-  log?: boolean;
   flipped?: boolean;
 };
 
 export default function ThemeProvider({
   children,
-  log = false,
   flipped = false,
 }: ThemeProviderProps) {
-  const scheme = useColorScheme();
+  // const scheme = useColorScheme(); TODO add this back once the light mode is ready
+  const scheme = "dark"; // TODO remove this once the light mode is ready
   const inhereted = useContext(ThemeContext);
   const theme = useMemo(() => {
     if (flipped) {
